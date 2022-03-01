dac395faa07676cd19d3b55fb36a4c1c7ab56269
A00817810 <A00817810@itesm.mx>
2022-03-01 01:30:44 -0600

    Samples
    Folder de Samples y una escena con un GameObject que referencia otro usando ExternalReference

---
+++ Samples/Scripts/SampleComponent.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HyperGnosys.Core;
+public class SampleComponent : MonoBehaviour
+{
+    [SerializeField] private ExternalReference<SamplePropertyInterface> propertyReference;
+}
