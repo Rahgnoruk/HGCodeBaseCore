bcaaf4bda17f45d2765dfceb56c286ec03a6e351
Rahgnoruk <tono.raimond@hotmail.com>
2022-03-14 14:20:23 -0600

    Debug Log en TaggedPropertyBehaviour
    Mando al Log los valores de las ExternalizableTaggedProperties para que se pueda observar que se acceden correctamente.

--- Samples/Scripts/TaggedPropertyBehaviour.cs
+++ Samples/Scripts/TaggedPropertyBehaviour.cs
@@ -6,4 +6,12 @@ public class TaggedPropertyBehaviour : MonoBehaviour
     [SerializeField] private ExternalizableTaggedProperty<float> sampleLocalFloat;
     [SerializeField] private ExternalizableTaggedProperty<float> sampleComponentFloat;
     [SerializeField] private ExternalizableTaggedProperty<float> sampleScriptableFloat;
+
+    private void Awake()
+    {
+        Debug.Log("String Value: " + sampleString.Value);
+        Debug.Log("Local Float Value: " + sampleLocalFloat.Value);
+        Debug.Log("Component Value: " + sampleComponentFloat.Value);
+        Debug.Log("Scriptable Value: " + sampleScriptableFloat.Value);
+    }
 }
